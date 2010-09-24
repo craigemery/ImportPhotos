7ddc3e9cc1f0a1f11163e098ed70923e115a85bd
Craig Emery <github@cemery.org.uk>
2010-09-24 16:30:51 +0100

    Now uses wxPython, hung off IDLE event, goes not responding some times, not ideal but works

--- importPhotos.py
+++ importPhotos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Copyright (c) 2009 Norman Craig Emery
+#Copyright (c) 2009-2010 Norman Craig Emery
 #
 #Permission is hereby granted, free of charge, to any person
 #obtaining a copy of this software and associated documentation
@@ -30,7 +30,7 @@ import os
 import string
 import shutil
 
-dest_root = os.path.join(os.environ['USERPROFILE'], 'Documents', 'My Pictures')
+import wx
 
 def get_date(dir, base, suff):
     if '.mov' == suff.lower():
@@ -47,43 +47,73 @@ def get_date(dir, base, suff):
         else:
           return None
 
-if __name__ == "__main__":
-    suffixes = ['.jpg', '.jpeg', '.mov']
-    images = {}
-    image_count = 0
-    count = 0
-    for dp, dn, fn in os.walk(sys.argv[1]):
-        count += len(fn)
-    print "Inspecting %s file%s" % (count, ("" if count == 1 else "s"))
-    for dp, dn, fn in os.walk(sys.argv[1]):
-        for f in fn:
-            b, s = os.path.splitext(f)
-            if s.lower() in suffixes:
-                p = os.path.join(dp, f)
-                gd = get_date(dp, b, s)
-                if gd:
-                  image_count += 1
-                  (year, month, day, hour, min, sec) = gd
-                  key = (year, month, day)
-                  l = images.get(key, [])
-                  l.append([dp, f])
-                  images[key] = l
+class MyFrame(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title, source_dir):
+        wx.Frame.__init__(self, parent, title=title, size=(800, 400))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.source_dir = source_dir
+        self.msg("Importing photos from %s" % (source_dir,))
+        self.control.SetEditable(False)
+        self.Show(True)
+        self.Bind(wx.EVT_IDLE, self.do_import)
 
-    print "Found %s image%s" % (image_count, ("" if image_count == 1 else "s"))
-    keys = images.keys()
-    keys.sort()
-    n = 0
-    for key in keys:
-        (year, month, day) = key
-        files = images[key]
-        d = os.path.join(dest_root, year, '%s_%s_%s' % (year, month, day))
-        if not os.path.isdir(d):
-            print "Creating directory", d
-            os.makedirs(d)
-        for (dp, f) in files:
-            n += 1
-            dest = os.path.join(d, f)
-            if not os.path.isfile(dest):
+    def msg(self, s):
+        #print s
+        self.control.AppendText("%s\n" % (s,))
+
+    def do_import(self, event):
+        self.Bind(wx.EVT_IDLE, None)
+        suffixes = ['.jpg', '.jpeg', '.mov']
+        image_details = []
+        images = {}
+        # First find all the image files
+        for dp, dn, fn in os.walk(self.source_dir):
+            for f in fn:
+                b, s = os.path.splitext(f)
+                if s.lower() in suffixes:
+                    image_details.append((dp, b, s, f))
+        # Now examine their EXIF data, if we can
+        image_count = len(image_details)
+        exif_count = 0
+        self.msg("Found %s image%s\nNow inspecting EXIF data" % (image_count, ("" if image_count == 1 else "s")))
+        for dp, b, s, f in image_details:
+            gd = get_date(dp, b, s)
+            if gd:
+                exif_count += 1
+                (year, month, day, hour, min, sec) = gd
+                key = (year, month, day)
+                l = images.get(key, [])
+                l.append([dp, f])
+                images[key] = l
+
+        self.msg("Found EXIF data in %s image%s" % (exif_count, ("" if exif_count == 1 else "s")))
+        keys = images.keys()
+        keys.sort()
+        n = 0
+        dest_root = os.path.join(os.environ['USERPROFILE'], 'Documents', 'My Pictures')
+        for key in keys:
+            (year, month, day) = key
+            files = images[key]
+            d = os.path.join(dest_root, year, '%s_%s_%s' % (year, month, day))
+            if not os.path.isdir(d):
+                self.msg("Creating directory %s" % (d,))
+                os.makedirs(d)
+            else:
+                self.msg("Directory %s already exists" % (d,))
+            for (dp, f) in files:
+                n += 1
+                dest = os.path.join(d, f)
                 src = os.path.join(dp, f)
-                print "Copying file [%d of %d] %s to directory %s" % (n, image_count, src, d)
-                shutil.copy2(src, d)
+                if not os.path.isfile(dest):
+                    self.msg("Importing photo [%d of %d] %s" % (n, exif_count, src))
+                    shutil.copy2(src, d)
+                else:
+                    self.msg("Photo [%d of %d] %s already imported" % (n, exif_count, src))
+        self.msg("All done!")
+
+if __name__ == "__main__" and len(sys.argv) > 1:
+    app = wx.App(False)
+    frame = MyFrame(None, "Craig's Photo Importer", sys.argv[1])
+    frame.Show(True)
+    app.MainLoop()
